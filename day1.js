fb441d0b6de196bf6af368576ce3f0ccbc76cc69
Shahbaz Liaquat <102903848+itechshahbaz@users.noreply.github.com>
2024-07-14 17:05:56 +0500

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,3 +1,66 @@
-// Day 1:
-	var myNum = 5500
-	console.log(myNum)
+//    ************* Day 1: ****************
+
+// Variable Declaration
+
+// var dec
+var myNum = 5500
+console.log(myNum)
+
+// let dec
+let myName = "Shahbaz Liaquat"
+console.log(myName)
+
+// const dec
+const mName = true
+console.log(mName)
+
+// different data types with typeof
+
+let urName = "something"
+console.log(typeof(urName))
+
+let urNumb = 1234
+console.log(typeof(urNumb))
+
+let urAge = false
+console.log(typeof(urAge))
+
+// reassign using let
+
+let myAgeIs = 18
+console.log(myAgeIs)
+
+myAgeIs = 26
+console.log(myAgeIs)
+
+// reassign values using const
+
+const urAgeIs = 30
+console.log(urAgeIs)
+
+urAgeIs = 18
+console.log(urAgeIs)
+// once a value in constant saved, it can't changed, because value in contant is fixed
+
+// now next
+
+// decleare variables, shows values and datatypes of variables
+
+let urFirstName = "Shahbaz"
+console.log(urFirstName)
+console.log("Datatype of Shahbaz is " + typeof(urFirstName))
+
+// same of see value and datatype of other variables,
+// also we have different methods for this, but for now we go only with this
+
+// reasignment script for let and const
+
+// it is simple, the value in constant is fixed so once we declare, we can't able to change
+// but the value of let is not fixed, it is changeable
+// we write code for both of these above
+
+
+
+
+//           ********* END of Day 1 Challenge ************ 
+
